f3b3c884da6474c1aaa0dfca4eba77fcec789b51
salman nazeer <nazeersalman978@gmail.com>
2024-05-14 16:23:17 +0500

    box added

--- app/src/main/java/com/example/supreheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/supreheroes/HeroesScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,8 +12,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -23,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -46,6 +50,7 @@ fun HeroItem(modifier: Modifier = Modifier, hero: Hero) {
                 .fillMaxWidth()
                 .heightIn(dimensionResource(id = R.dimen.image_size))
                 .padding(dimensionResource(id = R.dimen.padding_medium))
+                .sizeIn(minHeight = 78.dp)
         ) {
             Column(
                 modifier = Modifier.weight(1f)
@@ -59,15 +64,19 @@ fun HeroItem(modifier: Modifier = Modifier, hero: Hero) {
                     style = MaterialTheme.typography.bodyLarge
                 )
             }
-            Image(
+            Box(
                 modifier = Modifier
-                    .size(dimensionResource(id = R.dimen.image_size))
+                    .size(72.dp)
                     .clip(MaterialTheme.shapes.small)
-                    .padding(start = dimensionResource(id = R.dimen.padding_medium)),
+            ) {
+                Image(
+                    painter = painterResource(id = hero.imageRes),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    alignment = Alignment.TopCenter
+                )
+            }
 
-                painter = painterResource(id = hero.imageRes),
-                contentDescription = null
-            )
         }
     }
 }
@@ -91,6 +100,7 @@ fun HeroesAppBar(modifier: Modifier = Modifier) {
 
 
 }
+
 @Composable
 fun HeroesItemsList(modifier: Modifier = Modifier) {
     val list = HeroesRepository.heroes
